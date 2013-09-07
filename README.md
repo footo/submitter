bf9771a3aa3b168c590f42c6a39b83d4fd9acd83
footo <footo@outlook.com>
2013-09-07 18:53:31 +0800

    Initial & poj submitter

---
+++ README.md
@@ -0,0 +1,6 @@
+A Tool to Submit Codes and ...
+===
+
+This is a simple tool i designed to submit some codes to OJs (now support [POJ](http://poj.org)).
+
+I do have some *further* ideas.
---
+++ poj/submitter.rb
@@ -0,0 +1,45 @@
+require 'mechanize'
+
+module POJSmt
+  def initialize
+    @agent = Mechanize.new
+    @agent.get("http://poj.org")
+    @agent.post("http://poj.org/login",{
+        'user_id1'=>"submittersubmitter",
+        'password1'=>"123456",
+        'B1'=>'login'})
+      @agent.cookie_jar.save_as("cookie")
+  end
+
+  def get_run_id(id, language=0)
+    page=@agent.get('http://poj.org/status?problem_id='+id.to_s+
+            '&user_id=submittersubmitter&language='+language.to_s)
+    status=page.body
+    /\d+/.match(/<tr align=center><td>\d+<\/td>/.match(status).to_s).to_s.to_i
+  end
+
+  def submit(id, path, language=0)
+    code=""
+    File.open(path) do |file|
+      file.each_line{|line|code+=line}
+    end
+
+   
+    page=@agent.get("http://poj.org/submit")
+    f=page.form_with(:method=>"POST")
+    f['problem_id']=id.to_s
+    f['source']=code
+    f.click_button
+
+    get_run_id(id, language)
+  end
+end
+
+class Smt
+  include POJSmt
+end
+
+smt=Smt.new
+#smt.submit(1000,"tp.cpp")
+#puts smt.get_run_id(1000)
+
