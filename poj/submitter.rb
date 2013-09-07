ff1a40739ab14e536a42c4aa9228a99d762c70cb
footo <footo@outlook.com>
2013-09-07 20:32:38 +0800

    Adding trace function for POJ submitter

--- poj/submitter.rb
+++ poj/submitter.rb
@@ -1,14 +1,16 @@
 require 'mechanize'
 
-module POJSmt
+class POJSmt
   def initialize
+    puts "Initialize POJSmt."
     @agent = Mechanize.new
     @agent.get("http://poj.org")
     @agent.post("http://poj.org/login",{
         'user_id1'=>"submittersubmitter",
         'password1'=>"123456",
         'B1'=>'login'})
-      @agent.cookie_jar.save_as("cookie")
+    @agent.cookie_jar.save_as("cookie")
+    puts "Comlete."
   end
 
   def get_run_id(id, language=0)
@@ -24,7 +26,6 @@ module POJSmt
       file.each_line{|line|code+=line}
     end
 
-   
     page=@agent.get("http://poj.org/submit")
     f=page.form_with(:method=>"POST")
     f['problem_id']=id.to_s
@@ -33,13 +34,25 @@ module POJSmt
 
     get_run_id(id, language)
   end
+
+  def get_status(name,res)
+    tr=/#{name}.+<\/td>/.match(res)
+    tr=/>[a-zA-Z0-9 \/]{4,}</.match(tr.to_s)
+    /[a-zA-Z0-9 \/]{4,}/.match(tr.to_s)
+  end
+
+  def trace_status(id)
+    res=@agent.get('http://poj.org/showsource?solution_id='+id.to_s).body
+#puts(status)
+    status={}
+    status[:result]=get_status('Result',res)
+    status[:memory]=get_status('Memory',res)
+    status[:time]=get_status('Time',res)
+    status
+  end
 end
 
-class Smt
-  include POJSmt
-end
-
-smt=Smt.new
+smt=POJSmt.new
 #smt.submit(1000,"tp.cpp")
 #puts smt.get_run_id(1000)
-
+puts smt.trace_status(12086438)
